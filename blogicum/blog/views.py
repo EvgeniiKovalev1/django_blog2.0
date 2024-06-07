ecf4b34f594f6439b91df052fab647e7bc9a721b
Женя <zkovalev32@gmail.com>
2024-06-07 20:47:18 +0300

    fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,42 +1,39 @@
 from django.conf import settings
+
 from django.http import HttpResponse, HttpRequest
+
 from django.shortcuts import render, get_object_or_404
+
 from django.utils.timezone import now
 
 from blog.models import Post, Category
 
 
-def get_posts(**filters):
-    return Post.objects.select_related(
-        'author', 'category', 'location',
-    ).filter(
+def get_posts(category):
+    return Post.objects.select_related("author", "category", "location").filter(
         is_published=True,
         pub_date__lt=now(),
         category__is_published=True,
-        **filters
+        category=category,
     )
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    posts = get_posts()[:settings.POSTS_BY_PAGE]
-    template = 'blog/index.html'
-    context = {'post_list': posts}
-    return render(request, template, context)
+    posts = get_posts()[: settings.POSTS_BY_PAGE]
+    context = {"post_list": posts}
+    return render(request, "blog/index.html", context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(get_posts(), id=post_id)
-    return render(request, 'blog/detail.html', {'post': post})
+    return render(request, "blog/detail.html", {"post": post})
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(
-        Category, slug=category_slug, is_published=True)
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
     posts = get_posts(category=category)
-    template = 'blog/category.html'
     context = {
-        'category': category,
-        'post_list': posts,
-        'slug': category_slug,
+        "category": category,
+        "post_list": posts,
     }
-    return render(request, template, context)
+    return render(request, "blog/category.html", context)

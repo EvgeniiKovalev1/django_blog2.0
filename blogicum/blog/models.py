1bb2278d078951adc40fa446f1904ffcbe32ec29
Женя <zkovalev32@gmail.com>
2024-06-08 11:54:09 +0300

    fix

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,93 +1,86 @@
 from django.conf import settings
 
-from django.contrib.auth import get_user_model
-
 from django.db import models
+
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
 class BlogModel(models.Model):
     is_published = models.BooleanField(
-        "Опубликовано",
+        'Опубликовано',
         default=True,
-        help_text="Снимите галочку, чтобы скрыть публикацию.",
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
-    created_at = models.DateTimeField("Добавлено", auto_now_add=True)
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
 
     class Meta:
         abstract = True
-        ordering = ("created_at",)
+        ordering = ('created_at', )
 
 
 class Location(BlogModel):
     name = models.CharField(
-        "Название места",
-        max_length=settings.MAX_FIELD_LENGTH
-    )
+        'Название места', max_length=settings.MAX_FIELD_LENGTH)
 
-    class Meta:
-        verbose_name = "местоположение"
-        verbose_name_plural = "Местоположения"
+    class Meta(BlogModel.Meta):
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
     def __str__(self) -> str:
-        return self.name[: settings.REPRESENTATION_LENGTH]
+        return self.name[:settings.REPRESENTATION_LENGTH]
 
 
 class Category(BlogModel):
-    title = models.CharField("Заголовок", max_length=settings.MAX_FIELD_LENGTH)
-    description = models.TextField("Описание")
+    title = models.CharField('Заголовок', max_length=settings.MAX_FIELD_LENGTH)
+    description = models.TextField('Описание')
     slug = models.SlugField(
-        "Идентификатор",
+        'Идентификатор',
         max_length=settings.MAX_FIELD_LENGTH,
         unique=True,
         help_text=(
-            "Идентификатор страницы для URL; "
-            "разрешены символы латиницы, цифры, "
-            "дефис и подчёркивание."
-        ),
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, '
+            'дефис и подчёркивание.'
+        )
     )
 
-    class Meta:
-        verbose_name = "категория"
-        verbose_name_plural = "Категории"
+    class Meta(BlogModel.Meta):
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self) -> str:
-        return self.title[: settings.REPRESENTATION_LENGTH]
+        return self.title[:settings.REPRESENTATION_LENGTH]
 
 
 class Post(BlogModel):
-    title = models.CharField("Заголовок", max_length=settings.MAX_FIELD_LENGTH)
-    text = models.TextField("Текст")
+    title = models.CharField('Заголовок', max_length=settings.MAX_FIELD_LENGTH)
+    text = models.TextField('Текст')
     pub_date = models.DateTimeField(
-        "Дата и время публикации",
+        'Дата и время публикации',
         help_text=(
-            "Если установить дату и время в будущем — можно "
-            "делать отложенные публикации."
-        ),
+            'Если установить дату и время в будущем — можно '
+            'делать отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name="Автор публикации"
+        User, on_delete=models.CASCADE, verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
-        Location,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        verbose_name="Местоположение",
+        Location, on_delete=models.SET_NULL, null=True, blank=True,
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        null=True,
-        verbose_name="Категория"
+        Category, on_delete=models.SET_NULL, null=True,
+        verbose_name='Категория'
     )
 
     class Meta:
-        default_related_name = "posts"
-        verbose_name = "публикация"
-        verbose_name_plural = "Публикации"
-        ordering = ("-pub_date", *BlogModel.Meta.ordering)
+        default_related_name = 'posts'
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date', *BlogModel.Meta.ordering)
 
     def __str__(self):
-        return self.title[: settings.REPRESENTATION_LENGTH]
+        return self.title[:settings.REPRESENTATION_LENGTH]
